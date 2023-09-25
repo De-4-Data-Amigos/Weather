a1e7162568718f49fd026203b84c60040a424713
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2023-09-25 12:05:25 +0200

    crud

--- src/main/java/dao/WeatherDAOImpl.java
+++ src/main/java/dao/WeatherDAOImpl.java
@@ -1,6 +1,12 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
+import model.Weather;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class WeatherDAOImpl implements IWeatherDAO {
 
@@ -15,11 +21,43 @@ public class WeatherDAOImpl implements IWeatherDAO {
         return instance;
     }
 
-    //CREATE
+    @Override
+    public void create(Weather weather) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(weather);
+            em.getTransaction().commit();
+        }
 
-    //READ
+    }
 
-    //UPDATE
+    @Override
+    public Weather update(Weather weather) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.merge(weather);
+            em.getTransaction().commit();
+        }
+        return weather;
+    }
 
-    //DELETE
+    @Override
+    public Weather getById(int id) {
+        Weather weather = null;
+        try (var em = emf.createEntityManager()) {
+            Query query = em.createQuery("SELECT w FROM Weather w WHERE w.id = :id");
+            query.setParameter("id", id);
+            return weather = (Weather) query.getSingleResult();
+        }
+    }
+
+
+    @Override
+    public List<Weather> getAll() {
+        Weather[] weathers = null;
+        try (var em = emf.createEntityManager()) {
+            Query query = em.createQuery("SELECT w FROM Weather w");
+            return query.getResultList();
+        }
+    }
 }
